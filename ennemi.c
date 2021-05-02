734e98db8e79f3370f7e663507e683091f93a50e
boh10 <ahmed.mtibaa@esprit.tn>
2021-05-02 17:34:55 +0100

    Add files via upload

--- ennemi.c
+++ ennemi.c
@@ -17,36 +17,36 @@
 #include<unistd.h>
 #define LARGEUR_FENETRE 1500
 #define HAUTEUR_FENETRE 700
-#define VITESSE_INIT_VOIT 70// vitesse de ennemi 
-#define NB_VOIES 3//combien de animation de voiture 
+#define VITESSE_INIT_VOIT 70// vitesse de ennemi
+#define NB_VOIES 3//combien de animation d'ennemi
 #define LARG_enemi 175
 #define LONG_enemi 175
-#define MAX_enemi   1900 
-int  collision(SDL_Rect poss,Ennemy *e)
+#define MAX_enemi 1900
+int collision(SDL_Rect poss,Ennemy *e)
 {
-  
-//int nb=3 ;
-if 
-((poss.y>e->Position.y + e->Position.h ) ||( poss.y+poss.h< e->Position.y )||(poss.x+poss.w<e->Position.x) || (poss.x>e->Position.x + e->Position.w ))
- {
 
-  return 0 ;
- // return 0 ;
- }else
- {
- return 1 ;
- } 
- /*else
- { 
- return 1 ; 
-  //return 1 ; 
- }*/
-}
-void Initialisation_Ennemy(Ennemy * e,perso * perso)
+//int nb=3 ;
+if
+((poss.y>e->Position.y + e->Position.h ) ||( poss.y+poss.h< e->Position.y )||(poss.x+poss.w<e->Position.x) || (poss.x>e->Position.x + e->Position.w ))
 {
-int n  ; 
-//Ennemy en [MAX_enemi] ; 
-   srand(time(NULL)) ; 
+
+return 0 ;
+// return 0 ;
+}else
+{
+return 1 ;
+}
+/*else
+{
+return 1 ;
+//return 1 ;
+}*/
+}
+void Initialisation_Ennemy(Ennemy * e)
+{
+int n ;
+//Ennemy en [MAX_enemi] ;
+srand(time(NULL)) ;
 e->Position.x=750;
 e->Position.y=0;
 
@@ -56,7 +56,7 @@ e->state=WAITING;//tache blanche
 e->distance=300;
 //e->distance_a=300;
 
-//e->image=IMG_ Load("voiture") ; 
+//e->image=IMG_ Load("astro") ;
 e->Animation_D[1]=IMG_Load("1g.png");
 e->Animation_D[2]=IMG_Load("2g.png");
 e->Animation_D[3]=IMG_Load("3g.png");
@@ -64,21 +64,21 @@ e->Animation_D[3]=IMG_Load("3g.png");
 //e->Animation_G[1]=IMG_Load("1g.png");
 //e->Animation_G[2]=IMG_Load("2g.png");
 //e->Animation_G[3]=IMG_Load("3g.png");
-/*for (n=0; n<NB_VOIES; n++) 
-    
-        e->xvoies[n] = (LARGEUR_FENETRE / NB_VOIES /2) + ((LARGEUR_FENETRE / NB_VOIES) * n)- (LARG_enemi/ 3); // 2= position de l'ennemi par rapp au perso
-    
-    e->vitesse = VITESSE_INIT_VOIT;
-    e->y = 0; // 0= pos de descente de ennemi
- for (n=0; n<MAX_enemi; n++) {
-        en[n].voie = rand() % NB_VOIES;
-        en[n].y = (LONG_enemi + (LONG_enemi * 4) ) *(n + 1); // la descente dans chaque voie
-        en[n].Position.w = LARG_enemi;
-        en[n].Position.h = LONG_enemi;
-    }*/
+/*for (n=0; n<NB_VOIES; n++)
+
+e->xvoies[n] = (LARGEUR_FENETRE / NB_VOIES /2) + ((LARGEUR_FENETRE / NB_VOIES) * n)- (LARG_enemi/ 3); // 2= position de l'ennemi par rapp au perso
+
+e->vitesse = VITESSE_INIT_VOIT;
+e->y = 0; // 0= pos de descente de ennemi
+for (n=0; n<MAX_enemi; n++) {
+en[n].voie = rand() % NB_VOIES;
+en[n].y = (LONG_enemi + (LONG_enemi * 4) ) *(n + 1); // la descente dans chaque voie
+en[n].Position.w = LARG_enemi;
+en[n].Position.h = LONG_enemi;
+}*/
 }
 
-void Display_Ennemy(Ennemy *e,SDL_Surface *ecran)
+void afficher_Ennemy(Ennemy *e,SDL_Surface *ecran)
 {
 /*if(e->Direction)
 {
@@ -86,47 +86,50 @@ SDL_BlitSurface(e->Animation_D[e->Animation], NULL, ecran, &e->Position);
 //SDL_Flip(ecran);
 }
 else
-{*/ // e->Animation++ ; 
+{*/ // e->Animation++ ;
 SDL_BlitSurface(e->Animation_D[e->Animation], NULL, ecran, &e->Position);
 //SDL_Flip(ecran);
 //}
 }
 
-void Animation_Ennemy(Ennemy *e ,perso * perso)
+void Animation_Ennemy(Ennemy *e )
 {
-int n ; 
-   srand(time(NULL)) ; 
-//Ennemy en[MAX_enemi] ; 
- /*e->y += e->vitesse;
-    for (n=0; n<MAX_enemi ; n++) {
-     
+int n ;
+srand(time(NULL)) ;
+//Ennemy en[MAX_enemi] ;
+/*e->y += e->vitesse;
+for (n=0; n<MAX_enemi ; n++) {
 
-      if (abs(e[n].y - e->y) < HAUTEUR_FENETRE) {
-          e[n].visible = 1;
-          e[n].Position.x = e->xvoies[e[n].voie];//position aleatoire enemi
-            e[n].Position.y = HAUTEUR_FENETRE - (e[n].ys - e->y);//enemi deplacemnt aleatoire descend
-            
-       } else
-            e[n].visible = 0;
-    }*/
-  for (n=0; n<NB_VOIES; n++) 
+
+if (abs(e[n].y - e->y) < HAUTEUR_FENETRE) {
+e[n].visible = 1;
+e[n].Position.x = e->xvoies[e[n].voie];//position aleatoire enemi
+e[n].Position.y = HAUTEUR_FENETRE - (e[n].ys - e->y);//enemi deplacemnt aleatoire descend
+
+} else
+e[n].visible = 0;
+}*/
+for (n=0; n<NB_VOIES; n++)
 {
 
-       e->xvoies[n] = (LARGEUR_FENETRE / NB_VOIES /2) + ((LARGEUR_FENETRE / NB_VOIES) * n) ;// 2= position de l'ennemi par rapp au perso
+e->xvoies[n] = (LARGEUR_FENETRE / NB_VOIES /2) + ((LARGEUR_FENETRE / NB_VOIES) * n) ;// 2= position de l'ennemi par rapp au perso
 }
 if (e->Position.y==0)
 {
- e->voie = rand() % 3;
-  //e->Position.y = (LONG_enemi + (LONG_enemi * 1) ) *(n + 1);
-         e->Position.x = e->xvoies[e->voie];
-       }
-      if( collision(perso->poss,e)==0)
-      {
-      e->Position.y==0 ;
-      }
-int dist = (perso->poss.x - e->Position.x);
+e->voie = rand() % 3;
+//e->Position.y = (LONG_enemi + (LONG_enemi * 1) ) *(n + 1);
+e->Position.x = e->xvoies[e->voie];
+}
+/*if( collision(perso->poss,e)==0)
+{
+e->Position.y==0 ;
+}*/
+}
+void deplacer(Ennemy *e , SDL_Rect posperso)
+{
+int dist = (posperso.x - e->Position.x);
 //position aleatoire enemi
-if   ((dist > - e->distance) && (dist <  e->distance))
+if ((dist > - e->distance) && (dist < e->distance))
 {
 e->state=FOLLOWING;
 }
@@ -138,9 +141,9 @@ switch(e->state)
 {
 case WAITING:
 {
-if (e->Position.y==300) 
-{ 
-e->Position.y-=20 ; 
+if (e->Position.y==300)
+{
+e->Position.y-=20 ;
 }
 e->Animation++;
 
@@ -180,9 +183,9 @@ break;
 
 case FOLLOWING:
 {
-if (e->Position.y==300) 
-{ 
-e->Position.y-=20 ; 
+if (e->Position.y==300)
+{
+e->Position.y-=20 ;
 }
 e->Animation++;
 
@@ -200,7 +203,7 @@ else
 e->Position.y-=20;
 /*if(e->Position.y==300)
 {
-e->Position.y+=20 ; 
+e->Position.y+=20 ;
 } */
 }
 
@@ -218,39 +221,10 @@ return 0 ;
 }
 else
 {
-return 1 ; 
+return 1 ;
 }
 }*/
 
 
 
-/*void setrectss(SDL_Rect clips[9][4])
-{
-int j,i ;
-  for(int j=0;j<8;j++)
-  {
-    for (int i=0;i<4;i++)
-    {
-      clips[j][i].x=i*1536;
-      clips[j][i].y=j*864;
-      clips[j][i].h=864;
-      clips[j][i].w=1536;
-    }
-  }
-  for (int i=0;i<4;i++)
-  {
-    clips[8][i].x=i*1536;
-    clips[8][i].y=j*864;
-    clips[8][i].h=864;
-    clips[8][i].w=1536;
-  }
-}
-void Free_Ennemy(Ennemy *e)
-{
-for(int i=1;i<4;i++)
-{
-SDL_FreeSurface(e->Animation_D[i]);
-SDL_FreeSurface(e->Animation_G[i]);
-}
-}*/
 
--- main.c
+++ main.c
@@ -7,187 +7,188 @@
 *
 *Testing program for entite secondaire*
 */
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 //#include<unistd.h>
 //#include <SDL/SDL_mixer.h>
-//#include <SDL/SDL_ttf.h> 
+//#include <SDL/SDL_ttf.h>
 #include "ennemi.h"
 /* constantes */
-#define LARGEUR_FENETRE 1500 //largeur fenetre 
-#define HAUTEUR_FENETRE 700  //hauteur fenetre 
+#define LARGEUR_FENETRE 1500 //largeur fenetre
+#define HAUTEUR_FENETRE 700 //hauteur fenetre
 #define VITESSE_INIT_VOIT 70// vitesse de ennemi
 
-#define NB_VOIES 3 //combien de animation de voiture 
+#define NB_VOIES 3 //combien de animation d'ennemi
 #define LARG_enemi 175
 #define LONG_enemi 175
 
-#define MAX_enemi  1900 //
-int main(int argc ,char *argv[]) 
-{ 
-perso perso ; 
-  Ennemy ennemy  ;
+#define MAX_enemi 1900 //
+int main(int argc ,char *argv[])
+{
+perso perso ;
+Ennemy ennemy ;
 //int framej=0,frame=0,aux=0;
-SDL_Init(SDL_INIT_VIDEO) ; 
- fprintf(stderr,"EROOR SDL ;%s\n",SDL_GetError()) ; 
-SDL_Surface *screen=NULL ,*back=NULL,*astronaute=NULL,*enemii=NULL,*vaisseau1=NULL,*vaisseau2=NULL,*vaisseau3=NULL,*ar=NULL ; 
-screen=SDL_SetVideoMode(1500,700,24,SDL_HWSURFACE|SDL_DOUBLEBUF) ; 
-ar=IMG_Load("game.png") ; 
-back=IMG_Load("sprite.png") ; 
-vaisseau1=IMG_Load("g.png") ; 
-vaisseau2=IMG_Load("gg.png") ; 
-vaisseau3=IMG_Load("ggg.png") ; 
- //SDL_Rect rectss [9][4] ;
- SDL_Rect g,gg,ggg,pr ; 
- /*pr.x=0 ; 
- pr.y=0 ; */
- g.x=200 ; 
- g.y=0 ; 
- gg.x=1200 ; 
- gg.y=0 ;
- ggg.x=700 ; 
- ggg.y=0 ;  
+SDL_Init(SDL_INIT_VIDEO) ;
+fprintf(stderr,"EROOR SDL ;%s\n",SDL_GetError()) ;
+SDL_Surface *screen=NULL ,*back=NULL,*astronaute=NULL,*enemii=NULL,*vaisseau1=NULL,*vaisseau2=NULL,*vaisseau3=NULL,*ar=NULL ;
+screen=SDL_SetVideoMode(1500,700,24,SDL_HWSURFACE|SDL_DOUBLEBUF) ;
+ar=IMG_Load("game.png") ;
+back=IMG_Load("sprite.png") ;
+vaisseau1=IMG_Load("g.png") ;
+vaisseau2=IMG_Load("gg.png") ;
+vaisseau3=IMG_Load("ggg.png") ;
+//SDL_Rect rectss [9][4] ;
+SDL_Rect g,gg,ggg,pr ;
+/*pr.x=0 ;
+pr.y=0 ; */
+g.x=200 ;
+g.y=0 ;
+gg.x=1200 ;
+gg.y=0 ;
+ggg.x=700 ;
+ggg.y=0 ;
 //setrectss(rectss) ;
- perso.poss.y=400 ; 
-perso.poss.x=580  ;   
-perso.poss.h=392  ; 
-perso.poss.w=386 ; 
- //voiture
- astronaute=IMG_Load("astronaute.png") ; 
- //enemii=IMG_Load ("SS.png") ;  
- //SDL_Rect  poss ; 
-// perso.poss.x=580 ;  
- //perso.poss.y=400 ; 
-  
-  
- 
+perso.poss.y=400 ;
+perso.poss.x=580 ;
+perso.poss.h=392 ;
+perso.poss.w=386 ;
+//astrof
+astronaute=IMG_Load("astronaute.png") ;
+//enemii=IMG_Load ("SS.png") ;
+//SDL_Rect poss ;
+// perso.poss.x=580 ;
+//perso.poss.y=400 ;
 
-int continuer=1;  
-//int c=3 ;  
-SDL_Rect pos ; 
+
+
+
+int continuer=1;
+//int c=3 ;
+SDL_Rect pos ;
 Uint32 starttick;
-pos.x=0; 
-pos.y=0 ; 
+pos.x=0;
+pos.y=0 ;
 
 
-	//struct car car;
-    //struct enemi enemis[MAX_enemi];
-    //struct game game; 
-    Initialisation_Ennemy(&ennemy,&perso);
-    //init_objects(&game, &car, enemis,&perso);
-//SDL_Flip(screen) ; 
+//struct car car;
+//struct enemi enemis[MAX_enemi];
+//struct game game;
+Initialisation_Ennemy(&ennemy);
+//init_objects(&game, &car, enemis,&perso);
+//SDL_Flip(screen) ;
 const int fps=30;
 /*int n ;
-int nbre_vie ; 
-int score=3; 
-int nb=2 ;*/ 
- while(continuer) 
- { 
-  
-   starttick = SDL_GetTicks();
-  
-  
-        // move_objects(&game, &car, enemis );
-         Animation_Ennemy(&ennemy ,&perso);
+int nbre_vie ;
+int score=3;
+int nb=2 ;*/
+while(continuer)
+{
 
-  /*if (collision (enemis,&perso)==0)
-   {
-   return 0 ; 
-   }
-    //if(collision (enemis,&perso)!=0)
-  // {
-    //return 1 ; 
-   //}*/
+starttick = SDL_GetTicks();
 
 
-   
-  
-   
-   /*{
-   return 1 ; 
-   } else 
-   { 
-   return 1 ; 
-   }*/
-  /*{
-    nbre_vie--;
-   // score-- ; 
+// move_objects(&game, &car, enemis );
+Animation_Ennemy(&ennemy );
+ deplacer(&ennemy , perso.poss);
 
-  if(nbre_vie==0) 
-  {
-  return 0; 
-  }  else 
-  {
-  return 1 ; 
-  } 
-  }*/
-  SDL_BlitSurface(vaisseau1,NULL, screen,&g); 
-                                         SDL_BlitSurface(vaisseau2,NULL, screen,&gg); 
-                                       SDL_BlitSurface(vaisseau3,NULL, screen,&ggg); 
-   Display_Ennemy(&ennemy,screen) ;
+/*if (collision (enemis,&perso)==0)
+{
+return 0 ;
+}
+//if(collision (enemis,&perso)!=0)
+// {
+//return 1 ;
+//}*/
 
- // afficher(screen,voiture, enemii, enemis,&perso);    
-                   SDL_BlitSurface(astronaute,NULL, screen, &perso.poss);    
-                                      
+
+
+
+
+/*{
+return 1 ;
+} else
+{
+return 1 ;
+}*/
+/*{
+nbre_vie--;
+// score-- ;
+
+if(nbre_vie==0)
+{
+return 0;
+} else
+{
+return 1 ;
+}
+}*/
+SDL_BlitSurface(vaisseau1,NULL, screen,&g);
+SDL_BlitSurface(vaisseau2,NULL, screen,&gg);
+SDL_BlitSurface(vaisseau3,NULL, screen,&ggg);
+afficher_Ennemy(&ennemy,screen) ;
+
+// afficher(screen,astro, enemii, enemis,&perso);
+SDL_BlitSurface(astronaute,NULL, screen, &perso.poss);
+
 
 /*frame++;
- if (frame==3)
- {
- frame=0 ;
- framej++;
- }
+if (frame==3)
+{
+frame=0 ;
+framej++;
+}
 if (framej==8)
- {
- if (frame==2)
 {
- framej=0;
- frame=0;
- }
- }
+if (frame==2)
+{
+framej=0;
+frame=0;
+}
+}
 
-  framee++  ; 
-  if (framee==4) 
+framee++ ;
+if (framee==4)
 {
-     framee=0 ; 
-     }*/
-SDL_Event event  ; 
-while (SDL_PollEvent(&event))  
-{ 
-switch(event.type) 
-{ 
- case SDL_QUIT  : 
- { 
- return 0 ; 
- break; 
- } 
-  case SDL_KEYDOWN :
- { if(event.key.keysym.sym==SDLK_ESCAPE)
- {
-return 0 ;  
-}  
+framee=0 ;
+}*/
+SDL_Event event ;
+while (SDL_PollEvent(&event))
+{
+switch(event.type)
+{
+case SDL_QUIT :
+{
+return 0 ;
+break;
+}
+case SDL_KEYDOWN :
+{ if(event.key.keysym.sym==SDLK_ESCAPE)
+{
+return 0 ;
+}
 }
 // effzfzgzg
 
 if (event.key.keysym.sym==SDLK_RIGHT)
-{ 
+{
 perso.poss.x+=580;
 //aux++;
-/*if (aux==3) 
+/*if (aux==3)
 {
 //poss.y+=50 ;
 //poss.x=-300 ;
-//system("PAUSE") ;   
+//system("PAUSE") ;
 }*/
 
- //SDL_BlitSurface(image,&rects[framej][frame],screen,&pos) ;
+//SDL_BlitSurface(image,&rects[framej][frame],screen,&pos) ;
 
- //SDL_Flip(screen) ; 
+//SDL_Flip(screen) ;
 } else if (event.key.keysym.sym=SDLK_LEFT)
 {
 perso.poss.x-=580;
-//if (perso.poss.x==700) 
+//if (perso.poss.x==700)
 //{
 //perso.poss.x+=300;
 //}
@@ -195,71 +196,70 @@ perso.poss.x-=580;
 
 
 
-  
+
 }
 }
 
 
- SDL_BlitSurface(back,NULL,screen,&pos) ; 
-              SDL_BlitSurface(astronaute,NULL, screen, &perso.poss);
-              //int nb=2 ; 
-           
+SDL_BlitSurface(back,NULL,screen,&pos) ;
+SDL_BlitSurface(astronaute,NULL, screen, &perso.poss);
+//int nb=2 ;
 
-               
-               /*else 
-               {
-               continuer=1 ;
-               }
-               }else 
-               {
-               continuer= 0 ; 
-               }*/
-               
 
-     Display_Ennemy(&ennemy,screen) ;
-      SDL_BlitSurface(vaisseau1,NULL, screen,&g); 
-                                         SDL_BlitSurface(vaisseau2,NULL, screen,&gg); 
-                                       SDL_BlitSurface(vaisseau3,NULL, screen,&ggg); 
-if (collision (perso.poss,&ennemy)) 
-               {
-               //SDL_BlitSurface(back,NULL,screen,&pos) ; 
-               //ennemy.Position.x==0 ; 
-              // system("clear") ;
-                SDL_BlitSurface(ar,NULL, screen,&pr) ;
-               // while (SDL_PollEvent(&event))  
-//{ 
-/*switch(event.type) 
-{ 
-            case SDL_KEYDOWN :
- { 
- if(event.key.keysym.sym==SDLK_ESCAPE)
- 
- 
+
+/*else
+{
+continuer=1 ;
+}
+}else
+{
+continuer= 0 ;
+}*/
+
+
+afficher_Ennemy(&ennemy,screen) ;
+SDL_BlitSurface(vaisseau1,NULL, screen,&g);
+SDL_BlitSurface(vaisseau2,NULL, screen,&gg);
+SDL_BlitSurface(vaisseau3,NULL, screen,&ggg);
+if (collision (perso.poss,&ennemy))
+{
+//SDL_BlitSurface(back,NULL,screen,&pos) ;
+//ennemy.Position.x==0 ;
+// system("clear") ;
+SDL_BlitSurface(ar,NULL, screen,&pr) ;
+// while (SDL_PollEvent(&event))
+//{
+/*switch(event.type)
+{
+case SDL_KEYDOWN :
+{
+if(event.key.keysym.sym==SDLK_ESCAPE)
+
+
 {*/
-               continuer=1 ;
-              /* }
-               }
-               }
-               }*/
-               }
-               else 
-               {
-               continuer=1 ; 
-               }
-               
-  // afficher(screen,voiture,enemii, enemis,&perso);     
+continuer=1 ;
+/* }
+}
+}
+}*/
+}
+else
+{
+continuer=1 ;
+}
 
-		  SDL_Flip(screen);
-		  if(1000/fps>SDL_GetTicks()-starttick)
+// afficher(screen,astro,enemii, enemis,&perso);
+
+SDL_Flip(screen);
+if(1000/fps>SDL_GetTicks()-starttick)
 {
 SDL_Delay( 1000/fps-(SDL_GetTicks()-starttick) );
-} 
 }
-SDL_FreeSurface(back) ; 
-SDL_FreeSurface(astronaute) ; 
-SDL_FreeSurface(enemii) ; 
+}
+SDL_FreeSurface(back) ;
+SDL_FreeSurface(astronaute) ;
+SDL_FreeSurface(enemii) ;
 SDL_FreeSurface(screen) ; 
 SDL_Quit() ;
 return  0;   
 } 
-
